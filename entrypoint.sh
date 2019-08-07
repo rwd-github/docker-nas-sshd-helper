809459b21a0d2dd27a4f3ecb1bb685e57c9db32f
RWd <rwd-github@gmx.net>
2019-08-07 15:05:54 +0200

    -

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+rm -f /etc/ssh/ssh_host_* || echo ""
+
 ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
 ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
 ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
