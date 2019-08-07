f7031175a73409b75318677d7cfb3d54b03a2548
RWd <rwd-github@gmx.net>
2019-08-07 15:04:12 +0200

    -

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
 ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
 
 mkdir -p /root/.ssh
+rm /root/.ssh/authorized_keys || echo ""
 cp ${AUTHORIZED_KEYS} /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
 
