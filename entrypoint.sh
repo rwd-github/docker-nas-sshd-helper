59e7acb6864c3657a26676cb7bcfeea7805ef2f9
RWd <rwd-github@gmx.net>
2019-08-07 15:15:39 +0200

    -

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 
-rm -f /etc/ssh/ssh_host_* || echo ""
+cp -f ${KEYS}/ssh_host_* /etc/ssh/
+chown -R root. /etc/ssh
+cd /etc/ssh
+chmod 600 *_key
+chmod 644 *.pub
 
-ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
-ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
-ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
+
+#ssh-keygen -f /etc/ssh/ssh_host_rsa_key -N '' -t rsa
+#ssh-keygen -f /etc/ssh/ssh_host_ecdsa_key -N '' -t ecdsa
+#ssh-keygen -f /etc/ssh/ssh_host_ed25519_key -N '' -t ed25519
 
 mkdir -p /root/.ssh
-rm /root/.ssh/authorized_keys || echo ""
-cp ${AUTHORIZED_KEYS} /root/.ssh/authorized_keys
+cp -f ${KEYS}/authorized_keys /root/.ssh/authorized_keys
 chmod 600 /root/.ssh/authorized_keys
 
 /usr/sbin/sshd -D
